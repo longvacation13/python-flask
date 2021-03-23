1ae9e26cab434733108fd9ca4a626b0eb04890df
안준영(202172) <superukie@naver.com>
2021-03-23 17:05:49 +0900

    csv 파일 추가

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 # https://python-explorer.tistory.com/13
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__) 
 
 import numpy as np 
 import pandas as pd 
 import matplotlib.pyplot as plt
+import os
+import firebase_admin 
+from firebase_admin import credentials
 
+cred = credentials.Certificate("heroku-pro-firebase-adminsdk-na1xm-9e49a017fd.json")
+firebase_admin.initialize_app(cred)
 
-# print("hello world")
 
 @app.route('/')
 def hello_world(): 
- 
- return "Hello Python!"
+ print(os.getcwd)
+ return "hello python!"
  
 if __name__ == '__main__': 
  app.run() 
