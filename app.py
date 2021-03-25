2f471105d7881dd64814de76006c6cf132c2b970
안준영(202172) <superukie@naver.com>
2021-03-25 09:15:33 +0900

    # app.py에 firebase storage 사용 관련 코드 추가

--- app.py
+++ app.py
@@ -7,10 +7,22 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 import firebase_admin 
+from firebase import Firebase
 from firebase_admin import credentials
 
 cred = credentials.Certificate("heroku-pro-firebase-adminsdk-na1xm-9e49a017fd.json")
-firebase_admin.initialize_app(cred)
+config = {
+    apiKey: "AIzaSyA-fI8Z3W136-PAcGvc0GUFXGonoTpxmL4",
+    authDomain: "heroku-project-75f3a.firebaseapp.com",
+    databaseURL: "https://heroku-project-75f3a-default-rtdb.firebaseio.com",
+    projectId: "heroku-project-75f3a",
+    storageBucket: "heroku-project-75f3a.appspot.com",
+    messagingSenderId: "751329215750",
+    appId: "1:751329215750:web:69fd3dd5dbf1209ac4ca87",
+    measurementId: "G-H9V97HWFRQ"
+}
+# firebase_admin.initialize_app(cred)
+firebase = Firebase(config)
 
 
 @app.route('/')
